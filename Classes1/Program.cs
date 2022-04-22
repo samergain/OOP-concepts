7d30eafd9994c1298f8ae19a33b6d08957a90adc
samergain <samersadon@gmail.com>
2022-04-22 16:38:19 -0700

    adding concepts, methods, Fibonacci solutions.

---
+++ Classes1/Program.cs
@@ -0,0 +1,37 @@
+﻿/*
+ Designing and Building Classes using object-oriented principles
+1. Write a program that that demonstrates use of four basic principles of
+object-oriented programming /Abstraction/, /Encapsulation/, /Inheritance/ and
+/Polymorphism/.
+2. Use /Abstraction/ to define different classes for each person type such as Student
+and Instructor. These classes should have behavior for that type of person.
+3. Use /Encapsulation/ to keep many details private in each class.
+4. Use /Inheritance/ by leveraging the implementation already created in the Person
+class to save code in Student and Instructor classes.
+5. Use /Polymorphism/ to create virtual methods that derived classes could override to
+create specific behavior such as salary calculations.
+6. Make sure to create appropriate /interfaces/ such as ICourseService, IStudentService,
+IInstructorService, IDepartmentService, IPersonService, IPersonService (should have
+person specific methods). IStudentService, IInstructorService should inherit from
+IPersonService.
+Person
+Calculate Age of the Person
+Calculate the Salary of the person, Use decimal for salary
+Salary cannot be negative number
+Can have multiple Addresses, should have method to get addresses
+Instructor
+Belongs to one Department and he can be Head of the Department
+Instructor will have added bonus salary based on his experience, calculate his
+years of experience based on Join Date
+Student
+Can take multiple courses
+Calculate student GPA based on grades for courses
+Each course will have grade from A to F
+Course
+Will have list of enrolled students
+Department
+Will have one Instructor as head
+Will have Budget for school year (start and end Date Time)
+Will offer list of courses
+*/
+
---
+++ Methods/Program.cs
@@ -0,0 +1,71 @@
+﻿/*
+ Let’s make a program that uses methods to accomplish a task. Let’s take an array and
+reverse the contents of it. For example, if you have 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, it would
+become 10, 9, 8, 7, 6, 5, 4, 3, 2, 1.
+To accomplish this, you’ll create three methods: one to create the array, one to reverse the
+array, and one to print the array at the end.
+Your Main method will look something like this:
+static void Main(string[] args) {
+int[] numbers = GenerateNumbers();
+Reverse(numbers);
+PrintNumbers(numbers);
+}
+The GenerateNumbersmethod should return an array of 10 numbers. (For bonus points,
+change the method to allow the desired length to be passed in, instead of just always
+being 10.)
+The PrintNumbersmethod should use a foror foreachloop to print out each item in the
+array. The Reversemethod will be the hardest. Give it a try and see what you can make
+happen. If you get
+stuck, here’s a couple of hints:
+Hint #1:To swap two values, you will need to place the value of one variable in a temporary
+location to make the swap:
+// Swapping a and b.
+int a = 3;
+int b = 5;
+int temp = a;
+a = b;
+b = temp;
+Hint #2:Getting the right indices to swap can be a challenge. Use a forloop, starting at 0
+and going up to the length of the array / 2. The number you use in the forloop will be the
+index of the first number to swap, and the other one will be the length of the array minus
+the index minus 1. This is to account for the fact that the array is 0-based. So basically,
+you’ll be swapping array[index]with array[arrayLength – index – 1].
+*/
+
+static int[] GenerateNumbers(int size)
+{
+    Random random = new Random();
+    int[] numbers = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        numbers[i] = random.Next(0,100);
+    }
+    return numbers;
+}
+
+static int[] Reverse(int[] numbers)
+{
+    int temp;
+    for (int i = 0; i < numbers.Length/2; i++)
+    {
+        temp = numbers[i];
+        numbers[i] = numbers[numbers.Length - 1 - i];
+        numbers[numbers.Length - 1 - i] = temp;
+    }
+    return numbers;
+}
+
+static void PrintNumbers(int[] numbers)
+{
+    foreach(int number in numbers)
+    {
+        Console.WriteLine(number);
+    }
+}
+
+//Main
+    int[] numbers = GenerateNumbers(10);
+    Reverse(numbers);
+    PrintNumbers(numbers);
+
+
